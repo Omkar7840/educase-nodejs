e123e73d66f59b1b335058d6265024240b4d88a7
Omkar7840 <omkarbabar9421@gmail.com>
2026-03-31 22:58:54 +0530

    Added DB_PORT to database connection

--- db.js
+++ db.js
@@ -3,15 +3,18 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT, 
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+        rejectUnauthorized: false 
+    }
 });
 
-// Use promises for cleaner async/await syntax
 const promisePool = pool.promise();
 
 module.exports = promisePool;
